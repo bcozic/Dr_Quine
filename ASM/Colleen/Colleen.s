0a58d13e1c633b489e2057346fd927784b53effc
bcozic <bcozic@student.42.fr>
2019-02-13 20:05:12 +0100

    buffer too small in Grace.s (error close) and forget call function in Colleen.s

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -1,39 +1,49 @@
 section .data
 	s:
-		.s db "section .data%c	s:%c		.s db %c%s%c, 0%c%csection .text%c	global start%c	global _main%c	extern _printf%c%cstart:%c	call _main%c	ret%c%ccomment%c_main:%c	push rbp%c	mov rbp, rsp%c	sub rsp, 0x140%c%ccomment%c	lea rdi, [rel s.s]%c	mov rsi, 10%c	mov rdx, 10%c	mov rcx, 34%c	lea r8, [rel s.s]%c	mov r9, 34%c	xor rax, rax%c	mov r10, 40%cret_line:%c	mov QWORD [rsp+rax], 10%c	dec r10%c	add rax, 8%c	test r10, r10%c	jne ret_line%c	mov QWORD [rsp+80], 59%c	mov QWORD [rsp+128], 59%c	call _printf%c	add rsp, 0x140%c	xor rax, rax%c	leave%c	ret", 0
+		.s db "section .data%c	s:%c		.s db %c%s%c, 0%c%csection .text%c	global start%c	global _main%c	global _fnct%c	extern _printf%c%cstart:%c	call _main%c	ret%c%ccomment%c%c_fnct:%c	push rbp%c	mov rbp, rsp%c	mov QWORD [rdi+0x58], 59%c	mov QWORD [rdi+0xd0], 59%c	leave%c	ret%c%c_main:%c	push rbp%c	mov rbp, rsp%c	sub rsp, 0x200%c%ccomment%c	mov rsi, 10%c	mov rdx, 10%c	mov rcx, 34%c	lea r8, [rel s.s]%c	mov r9, 34%c	xor rax, rax%c	mov r10, 50%cret_line:%c	mov QWORD [rsp+rax], 10%c	dec r10%c	add rax, 8%c	test r10, r10%c	jne ret_line%c	mov rdi, rsp%c	call _fnct%c	lea rdi, [rel s.s]%c	call _printf%c	add rsp, 0x200%c	xor rax, rax%c	leave%c	ret%c", 0
 
 section .text
 	global start
 	global _main
+	global _fnct
 	extern _printf
 
 start:
 	call _main
 	ret
 ;comment
+
+_fnct:
+	push rbp
+	mov rbp, rsp
+	mov QWORD [rdi+0x58], 59
+	mov QWORD [rdi+0xd0], 59
+	leave
+	ret
+
 _main:
 	push rbp
 	mov rbp, rsp
-	sub rsp, 0x140
+	sub rsp, 0x200
 ;comment
-	lea rdi, [rel s.s]
 	mov rsi, 10
 	mov rdx, 10
 	mov rcx, 34
 	lea r8, [rel s.s]
 	mov r9, 34
 	xor rax, rax
-	mov r10, 40
+	mov r10, 50
 ret_line:
 	mov QWORD [rsp+rax], 10
 	dec r10
 	add rax, 8
 	test r10, r10
 	jne ret_line
-	mov QWORD [rsp+80], 59
-	mov QWORD [rsp+128], 59
+	mov rdi, rsp
+	call _fnct
+	lea rdi, [rel s.s]
 	call _printf
-	add rsp, 0x140
+	add rsp, 0x200
 	xor rax, rax
 	leave
 	ret
--- ASM/Grace/Grace.s
+++ ASM/Grace/Grace.s
@@ -2,7 +2,7 @@
 %macro BEGIN 0
 	section .data
 	s:
-		.s db "%cdefine SYSCALL(nb)		0x2000000 | nb%c%cmacro BEGIN 0%c	section .data%c	s:%c		.s db %c%s%c, 0%c		.file db %cGrace_kid.s%c%c	section .text%c	global start%c	global _main%c	extern _dprintf%c%c	start:%c		MAIN%c		ret%c%cendmacro%c%cmacro MAIN 0%c	_main:%c		push rbp%c		mov rbp, rsp%c		sub rsp, 0x260%c		lea rdi, [rel s.file]%c		mov rsi, 0x201%c		mov rdx, 0x180%c		mov rax, SYSCALL(5)%c		syscall%c		jc end_grace%c		mov QWORD [rbp-8], rax%c%ccomment%c		mov rdi, rax%c		lea rsi, [rel s.s]%c		mov rdx, 37%c		mov rcx, 10%c		mov r8, 37%c		mov r9, 10%c		xor rax, rax%c		mov r10, 76%c	ret_line:%c		mov QWORD [rsp+rax], 10%c		dec r10%c		add rax, 8%c		test r10, r10%c		jne ret_line%c		mov QWORD [rsp+0x10], 34%c		lea r11, [rel s.s]%c		mov QWORD [rsp+0x18], r11%c		mov QWORD [rsp+0x20], 34%c		mov QWORD [rsp+0x30], 34%c		mov QWORD [rsp+0x38], 34%c		mov QWORD [rsp+0x88], 37%c		mov QWORD [rsp+0x98], 37%c		mov QWORD [rsp+0x100], 59%c		mov QWORD [rsp+0x220], 37%c		call _dprintf%c		mov rdi, QWORD [rbp-8]%c		mov rax, SYSCALL(6)%c		syscall%c		jc end_grace%c		xor rax, rax%c	end_grace:%c		add rsp, 0x260%c		leave%c		ret%c%cendmacro%c%cBEGIN", 0
+		.s db "%cdefine SYSCALL(nb)		0x2000000 | nb%c%cmacro BEGIN 0%c	section .data%c	s:%c		.s db %c%s%c, 0%c		.file db %cGrace_kid.s%c%c	section .text%c	global start%c	global _main%c	extern _dprintf%c%c	start:%c		MAIN%c		ret%c%cendmacro%c%cmacro MAIN 0%c	_main:%c		push rbp%c		mov rbp, rsp%c		sub rsp, 0x270%c		lea rdi, [rel s.file]%c		mov rsi, 0x201%c		mov rdx, 0x180%c		mov rax, SYSCALL(5)%c		syscall%c		jc end_grace%c		mov QWORD [rbp-8], rax%c%ccomment%c		mov rdi, rax%c		lea rsi, [rel s.s]%c		mov rdx, 37%c		mov rcx, 10%c		mov r8, 37%c		mov r9, 10%c		xor rax, rax%c		mov r10, 76%c	ret_line:%c		mov QWORD [rsp+rax], 10%c		dec r10%c		add rax, 8%c		test r10, r10%c		jne ret_line%c		mov QWORD [rsp+0x10], 34%c		lea r11, [rel s.s]%c		mov QWORD [rsp+0x18], r11%c		mov QWORD [rsp+0x20], 34%c		mov QWORD [rsp+0x30], 34%c		mov QWORD [rsp+0x38], 34%c		mov QWORD [rsp+0x88], 37%c		mov QWORD [rsp+0x98], 37%c		mov QWORD [rsp+0x100], 59%c		mov QWORD [rsp+0x220], 37%c		call _dprintf%c		mov rdi, QWORD [rbp-8]%c		mov rax, SYSCALL(6)%c		syscall%c		jc end_grace%c		xor rax, rax%c	end_grace:%c		add rsp, 0x270%c		leave%c		ret%c%cendmacro%c%cBEGIN", 0
 		.file db "Grace_kid.s"
 	section .text
 	global start
@@ -17,7 +17,7 @@
 	_main:
 		push rbp
 		mov rbp, rsp
-		sub rsp, 0x260
+		sub rsp, 0x270
 		lea rdi, [rel s.file]
 		mov rsi, 0x201
 		mov rdx, 0x180
@@ -57,7 +57,7 @@
 		jc end_grace
 		xor rax, rax
 	end_grace:
-		add rsp, 0x260
+		add rsp, 0x270
 		leave
 		ret
 %endmacro
