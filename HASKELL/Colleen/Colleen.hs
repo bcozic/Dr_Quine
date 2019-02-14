4d6c9f1c1e30ebaa34a206e5ccf65c9c6e53d603
bcozic <bcozic@student.42.fr>
2019-02-14 16:27:02 +0100

    naming

--- HASKELL/Colleen/Colleen.hs
+++ HASKELL/Colleen/Colleen.hs
@@ -2,9 +2,9 @@ import Text.Printf
 
 --comment
 
-string :: (Integral a) => a -> String
-string 1 = "import Text.Printf\n\n--comment\n\nstring :: (Integral a) => a -> String\nstring 1 = %s\n\nmain = do\n    --comment\n    printf (string 1) (show (string 1))\n"
+get_string :: (Integral a) => a -> String
+get_string 1 = "import Text.Printf\n\n--comment\n\nget_string :: (Integral a) => a -> String\nget_string 1 = %s\n\nmain = do\n    --comment\n    printf (get_string 1) (show (get_string 1))\n"
 
 main = do
     --comment
-    printf (string 1) (show (string 1))
+    printf (get_string 1) (show (get_string 1))
--- HASKELL/Sully/Sully.hs
+++ HASKELL/Sully/Sully.hs
@@ -6,15 +6,15 @@ import System.Exit
 
 main = do
     let nb = 5
-    let pars_file = "Sully_%s.hs"
-    let file = printf pars_file (show (nb - 1))
-    let str = "import System.IO\nimport Data.Char\nimport Text.Printf\nimport System.Process\nimport System.Exit\n\nmain = do\n    let nb = %s\n    let pars_file = %s\n    let file = printf pars_file (show (nb - 1))\n    let str = %s\n    let pars_compile = %s\n    let compile = printf pars_compile file\n    let pars_run = %s\n    let run = printf pars_run (show (nb -1))\n    if nb <= 0\n        then exitWith ExitSuccess\n    else writeFile file (printf str (show (nb - 1)) (show pars_file) (show str) (show pars_compile) (show pars_run))\n    system compile\n    system run\n"
-    let pars_compile = "ghc --make %s"
-    let compile = printf pars_compile file
-    let pars_run = "./Sully_%s"
-    let run = printf pars_run (show (nb -1))
+    let parse_file = "Sully_%s.hs"
+    let file = printf parse_file (show (nb - 1))
+    let str = "import System.IO\nimport Data.Char\nimport Text.Printf\nimport System.Process\nimport System.Exit\n\nmain = do\n    let nb = %s\n    let parse_file = %s\n    let file = printf parse_file (show (nb - 1))\n    let str = %s\n    let parse_compile = %s\n    let compile = printf parse_compile file\n    let parse_run = %s\n    let run = printf parse_run (show (nb -1))\n    if nb <= 0\n        then exitWith ExitSuccess\n    else writeFile file (printf str (show (nb - 1)) (show parse_file) (show str) (show parse_compile) (show parse_run))\n    system compile\n    system run\n"
+    let parse_compile = "ghc --make %s"
+    let compile = printf parse_compile file
+    let parse_run = "./Sully_%s"
+    let run = printf parse_run (show (nb -1))
     if nb <= 0
         then exitWith ExitSuccess
-    else writeFile file (printf str (show (nb - 1)) (show pars_file) (show str) (show pars_compile) (show pars_run))
+    else writeFile file (printf str (show (nb - 1)) (show parse_file) (show str) (show parse_compile) (show parse_run))
     system compile
     system run
