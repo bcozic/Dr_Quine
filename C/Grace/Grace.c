e1c00b79da2604004f38f951fe9fbd1bebf98c26
bcozic <bcozic@student.42.fr>
2019-02-24 14:22:42 +0100

    return if fd == -1, split grace in multi lignes

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,7 +1,14 @@
-#define PROTO() int open(const char *path, int oflag, ...);int close(int fildes);int dprintf(int fd, const char * restrict format, ...);MAIN()
-#define S1 "#define PROTO() int open(const char *path, int oflag, ...);int close(int fildes);int dprintf(int fd, const char * restrict format, ...);MAIN()%c#define S1 %c%s%c%c#define MAIN() int main(){int fd = open(%cGrace_kid.c%c, 512 | 1, 256);dprintf(fd, S1, 10, 34, S1, 34, 10, 34, 34, 10, 10, 10, 10);close(fd);}%c/*%c	Comment%c*/%cPROTO()"
-#define MAIN() int main(){int fd = open("Grace_kid.c", 512 | 1, 256);dprintf(fd, S1, 10, 34, S1, 34, 10, 34, 34, 10, 10, 10, 10);close(fd);}
+#define BEGIN() int open(const char *path, int oflag, ...);\
+				int close(int fildes);\
+				int dprintf(int fd, const char * restrict format, ...);\
+				MAIN()
+#define S1 "#define BEGIN() int open(const char *path, int oflag, ...);%c%c				int close(int fildes);%c%c				int dprintf(int fd, const char * restrict format, ...);%c%c				MAIN()%c#define S1 %c%s%c%c#define MAIN() int main(){%c%c				int fd = open(%cGrace_kid.c%c, 512 | 1, 256);%c%c				if (fd == -1) return (1);%c%c				dprintf(fd, S1, 92, 10, 92, 10, 92, 10, 10, 34, S1, 34, 10, 92, 10, 34, 34, 92, 10, 92, 10, 92, 10, 10, 10, 10, 10, 10);%c%c				close(fd);}%c/*%c	Comment%c*/%cBEGIN()%c"
+#define MAIN() int main(){\
+				int fd = open("Grace_kid.c", 512 | 1, 256);\
+				if (fd == -1) return (1);\
+				dprintf(fd, S1, 92, 10, 92, 10, 92, 10, 10, 34, S1, 34, 10, 92, 10, 34, 34, 92, 10, 92, 10, 92, 10, 10, 10, 10, 10, 10);\
+				close(fd);}
 /*
 	Comment
 */
-PROTO()
+BEGIN()
--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -15,9 +15,11 @@ int main()
 	char file[16];
 	sprintf(file, "Sully_%d.c", nb - 1);
 	char cmd[512];
-	char *s = "#include <fcntl.h>%c#include <unistd.h>%c#include <stdio.h>%c#include <stdlib.h>%c#include <string.h>%c#include <sys/stat.h>%c%cint main()%c{%c	int nb = %d;%c	if (nb <= 0) return (0);%c#ifndef NOTFIRST%c	nb++;%c#endif%c	char file[16];%c	sprintf(file, %cSully_%cd.c%c, nb - 1);%c	char cmd[512];%c	char *s = %c%s%c;%c	int fd = open(file, O_CREAT | O_RDWR,S_IWUSR | S_IRUSR);%c	dprintf(fd,s,10,10,10,10,10,10,10,10,10,nb-1,10,10,10,10,10,10,34,37,34,10,10,34,s,34,10,10,10,10,34,37,34,10,34,37,37,34,10,10,34,37,34,10,10,10);%c	close(fd);%c	sprintf(file, %cSully_%cd%c, nb - 1); %c	sprintf(cmd, %cgcc -Wall -Wextra -Werror -DNOTFIRST -o %cs %cs.c%c, file, file);%c	system(cmd);%c	sprintf(cmd, %c./%cs%c, file);%c	system(cmd);%c}%c";
+	char *s = "#include <fcntl.h>%c#include <unistd.h>%c#include <stdio.h>%c#include <stdlib.h>%c#include <string.h>%c#include <sys/stat.h>%c%cint main()%c{%c	int nb = %d;%c	if (nb <= 0) return (0);%c#ifndef NOTFIRST%c	nb++;%c#endif%c	char file[16];%c	sprintf(file, %cSully_%cd.c%c, nb - 1);%c	char cmd[512];%c	char *s = %c%s%c;%c	int fd = open(file, O_CREAT | O_RDWR,S_IWUSR | S_IRUSR);%c	if (fd == -1)%c		return(1);%c	dprintf(fd,s,10,10,10,10,10,10,10,10,10,nb-1,10,10,10,10,10,10,34,37,34,10,10,34,s,34,10,10,10,10,10,10,34,37,34,10,34,37,37,34,10,10,34,37,34,10,10,10);%c	close(fd);%c	sprintf(file, %cSully_%cd%c, nb - 1); %c	sprintf(cmd, %cgcc -Wall -Wextra -Werror -DNOTFIRST -o %cs %cs.c%c, file, file);%c	system(cmd);%c	sprintf(cmd, %c./%cs%c, file);%c	system(cmd);%c}%c";
 	int fd = open(file, O_CREAT | O_RDWR,S_IWUSR | S_IRUSR);
-	dprintf(fd,s,10,10,10,10,10,10,10,10,10,nb-1,10,10,10,10,10,10,34,37,34,10,10,34,s,34,10,10,10,10,34,37,34,10,34,37,37,34,10,10,34,37,34,10,10,10);
+	if (fd == -1)
+		return(1);
+	dprintf(fd,s,10,10,10,10,10,10,10,10,10,nb-1,10,10,10,10,10,10,34,37,34,10,10,34,s,34,10,10,10,10,10,10,34,37,34,10,34,37,37,34,10,10,34,37,34,10,10,10);
 	close(fd);
 	sprintf(file, "Sully_%d", nb - 1); 
 	sprintf(cmd, "gcc -Wall -Wextra -Werror -DNOTFIRST -o %s %s.c", file, file);
